8daa60e5d32f02101867b27a87469ce7342be36b
Calvin Merrell <calvinmerrell@gmail.com>
2020-11-29 20:18:11 -0700

    studied like crazy

---
+++ src/components/TopicBrowser/TopicBrowser.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+
+
+export default class TopicBrowser extends Component {
+    render (){
+        return (
+            <h1>Hello World</h1>
+        )
+    }    
+}
+
+ 
